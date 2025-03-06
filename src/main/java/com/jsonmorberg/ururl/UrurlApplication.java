bae8a712649aec18a3121c4079ce0963e3c4e1e9
Jayson <jaysonmorberg9@gmail.com>
2025-03-06 21:59:28 +0000

    Initial commit w/ Spring starting files

---
+++ src/main/java/com/jsonmorberg/ururl/UrurlApplication.java
@@ -0,0 +1,13 @@
+package com.jsonmorberg.ururl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrurlApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrurlApplication.class, args);
+	}
+
+}
