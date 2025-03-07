35790c602a13c05682e7bbf989ac23aa00ef9886
Jayson <jaysonmorberg9@gmail.com>
2025-03-07 15:57:58 -0800

    Correct host locations

--- src/main/java/com/jsonmorberg/ururl/utils/RedisConfig.java
+++ src/main/java/com/jsonmorberg/ururl/utils/RedisConfig.java
@@ -16,7 +16,7 @@ public class RedisConfig {
 
     @Bean
     public RedisConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory("192.168.50.94", 6379);
+        return new LettuceConnectionFactory("localhost", 6379);
     }
 
     @Bean
